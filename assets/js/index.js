46b6e9719fdb2c1f7f4a3fb9461f0126c85921c4
Arizalb <139053552+Arizalb@users.noreply.github.com>
2024-12-07 23:42:44 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+//   function Readmore
+$(document).ready(function () {
+  $("#myBtn").click(function () {
+    $("#short").toggle("slow");
+    $("#more").slideToggle("slow");
+    var btnText = $("#myBtn").text();
+    if (btnText === "Read more") {
+      $("#myBtn").text("Read less");
+    } else {
+      $("#myBtn").text("Read more");
+    }
+  });
+});
